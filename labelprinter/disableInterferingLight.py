d0f0d7bc2653d0e9b6f167d294bd18d6881b5306
Rene Schneider <slartie@posteo.de>
2021-11-28 12:20:32 +0100

    added dummy return data

--- labelprinter/disableInterferingLight.py
+++ labelprinter/disableInterferingLight.py
@@ -3,3 +3,7 @@
 import requests
 
 requests.post(url = "http://homematic-raspi:2001/", data = "<methodCall><methodName>setValue</methodName><params><param><value><string>NEQ0399036:1</string></value></param><param><value><string>STATE</string></value></param><param><value><string>0</string></value></param></params></methodCall>")
+
+print("Content-Type: text/plain")
+print()
+print("success!")
